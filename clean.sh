22be5ec61a2523f6d83cc1b0a4752fec83627209
Császár Péter <peter.csaszar@blumsoft.eu>
2009-12-22 09:48:01 +0100

    fixed main clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-call datamodel/clean.sh || { exit }
+subdirs="datamodel "
 
+for dir in $subdirs; do
+	pushd $dir
+	echo
+	echo Cleaning in $dir
+	echo
+	./clean.sh || {
+		echo Error during $dir/clean.sh
+		echo Aborting cleaning
+		exit
+	}
+	popd
+done
+
+echo
 echo All clean done
+echo
 
