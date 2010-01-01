07965c56109a95a369f9e0c8292549d372854afd
Császár Péter <peter.csaszar@blumsoft.eu>
2010-01-01 23:24:23 +0100

    clean up test db file after testing

--- sqlite/test/sqlite/sqlite.cpp
+++ sqlite/test/sqlite/sqlite.cpp
@@ -6,6 +6,7 @@
  * Copyright (C) 2009 Csaszar, Peter
  */
 
+#include <QFile>
 #include <QtTest/QtTest>
 
 #include <ButlerDebug>
@@ -31,6 +32,9 @@ namespace Butler {
 
 	void TestSqlite::cleanupTestCase()
 	{
+		QFile f("./TestSqlite.db");
+		QVERIFY(f.remove());
+
 		_reportLeakSuspections();
 	}
 
