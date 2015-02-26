396639f69c1bfa7e0edc42132e65f6bbef068134
csjpeter <csjpeter@gmail.com>
2015-02-26 23:49:06 +0100

    generator work

--- source.mk
+++ source.mk
@@ -213,7 +213,7 @@ $(DIST_DIR)/src/butler_dataclasses.h: \
 		tools/generator \
 		src/butler_dataclasses.h
 	@test -d $(dir $@) || mkdir -p $(dir $@)
-	tools/generator -t ./tpl/ < src/butler_dataclasses.h > $@
+	#tools/generator -t ./tpl/ < src/butler_dataclasses.h > $@
 	./code-generator.py -t ./tpl/ < src/butler_dataclasses.h > $@
 
 $(DIST_DIR)/%: %
--- tools/generator.cpp
+++ tools/generator.cpp
@@ -7,9 +7,90 @@
 #include <csjp_logger.h>
 #include <csjp_file.h>
 
+using namespace csjp;
+
+class Generator
+{
+public:
+	String sqlTableName;
+	String sqlTableAutoIncField;
+	String localDbClass;
+	String remoteName;
+	String keyList;
+        String customizations;
+        String code;
+
+void include_CommonMarkerValues(StringChunk & tpl, unsigned & pos)
+{
+        if(tpl.startsWith("@TableName@")) {
+                tpl.chopFront(11);
+		tpl.trimFront("\n\r");
+		//code.trimBack("\t");
+                code << $sqlTableName;
+        }/* else if(substr($tpl, $pos, 19) == "@TableAutoIncField@") {
+                $pos += 19;
+                include_skip_newlines($tpl, $pos);
+                #$code = rtrim($code, "\t");
+                $code .= $sqlTableAutoIncField;
+        } else if(substr($tpl, $pos, 14) == "@LocalDbClass@") {
+                $pos += 14;
+                include_skip_newlines($tpl, $pos);
+                #$code = rtrim($code, "\t");
+                $code .= $localDbClass;
+        } else if(substr($tpl, $pos, 12) == "@RemoteName@") {
+                $pos += 12;
+                include_skip_newlines($tpl, $pos);
+                #$code = rtrim($code, "\t");
+                $code .= $remoteName;
+        } else if(substr($tpl, $pos, 9) == "@KeyList@") {
+                $pos += 9;
+                include_skip_newlines($tpl, $pos);
+                #$code = rtrim($code, "\t");
+                $code .= strtolower($keyList);
+        } else if(substr($tpl, $pos, 17) == "@RemoteNameLower@") {
+                $pos += 17;
+                include_skip_newlines($tpl, $pos);
+                #$code = rtrim($code, "\t");
+                $code .= strtolower($remoteName);
+        } else if(substr($tpl, $pos, 19) == "@CustomizationInit@") {
+                $pos += 19;
+                if(in_array("init", $customizations))
+                        $code .= "\$o->init();";
+                else {
+                        include_skip_newlines($tpl, $pos);
+                        $code = rtrim($code, "\t");
+                }
+        } else if(substr($tpl, $pos, 20) == "@CustomizationApply@") {
+                $pos += 20;
+                if(in_array("apply", $customizations))
+                        $code .= "\$this->apply();";
+                else {
+                        include_skip_newlines($tpl, $pos);
+                        $code = rtrim($code, "\t");
+                }
+        }*/ else {
+                code << tpl[0];
+                tpl.chopFront(1);
+        }
+}
+
+};
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc, char *args[])
 {
+	String tplDir;
+
 #ifdef DEBUG
 	csjp::verboseMode = true;
 #endif
@@ -40,6 +121,13 @@ int main(int argc, char *args[])
 			argi++;
 			continue;
 		}
+		if(1 <= argc - argi && (
+				  !strcmp(args[argi], "--tpldir") ||
+				  !strcmp(args[argi], "-t"))){
+			tplDir = args[argi+1];
+			argi += 2;
+			continue;
+		}
 
 		if(!strcmp(args[argi], "")) /* The rests are Qt arguments. */
 			break;
@@ -49,4 +137,5 @@ int main(int argc, char *args[])
 		return 1;
 	}
 
+	Generator generator;
 }
