fc47cf8023511ff6970eaf9f192a1eb447054199
csjpeter <csjpeter@gmail.com>
2015-03-01 14:29:30 +0100

    generator work

--- tools/generator.cpp
+++ tools/generator.cpp
@@ -37,225 +37,229 @@ bool operator<(const FieldDesc & a, const FieldDesc & b)
 	return a.name < b.name;
 }
 
-class Generator
+class Declaration
 {
-	enum InputState {
-		Start,
-		Declaration,
-		FieldList,
-		ConstraintList,
-		Include,
-		NumOfStates
-	};
-	enum TemplateState {
-		Start,
-		NumOfStates
-	};
+	void (const StringChunk &) state;
 public:
-	enum InputState inputState;
-	enum TemplateState templateState;
-	StringChunk tplDir;
-
 	StringChunk typeName;
 	Array<FieldDesc> fields;
 	Array<FieldDesc> keyFields;
 	Array<FieldDesc> nonKeyFields;
 	Array<String> constraints;
 
+	Declaration() { clear(); }
+
+	void clear()
+	{
+		state = parseDeclaration;
+		typeName.clear();
+		fields.clear();
+		keyFields.clear();
+		nonKeyFields.clear();
+		constraints.clear();
+	}
+
+	void parseFieldList(const StringChunk & line)
+	{
+		StringChunk comment;
+		if(line.findFirst(pos, "//")){
+			comment.assign(line.data + pos, line.length - pos);
+			line.cutAt(pos);
+		}
+		auto words = line.split(";");
+		if(words.length == 1 && words[0] == "}"){
+			state = parseDeclaration;
+			break;
+		}
+
+		StringChunk type;
+		StringChunk name;
+		if(0 < words.length){
+			words[0].trim(" \t");
+			if(words[0].findLastOf(pos, " \t")){
+				type.assign(words[0].data, pos);
+				words[0].chopFront(pos + 1);
+				name.assign(words[0]);
+			}
+		}
+
+		bool key = false;
+		if(1 < words.length){
+			auto modifiers = words[1].split(",");
+			key = modifiers.has("key");
+		}
+
+		StringChunk sql;
+		if(2 < words.length)
+			sql = words[2];
+
+		fields.add(name, type, sql, comment);
+		if(key)
+			keyFields.add(name, type, sql, comment);
+		else
+			nonKeyFields.add(name, type, sql, comment);
+	}
+
+	void parseConstraintList(const StringChunk & line)
+	{
+		auto words = line.split(";");
+		if(words.length == 1 && words[0] == "}"){
+			state = parseDeclaration;
+			break;
+		}
+
+		line.trim("\n ");
+		constraints.add(line);
+	}
+
+	void parseDeclaration(const StringChunk & line)
+	{
+		auto words = line.split(" \t");
+		if(words[0] == "Class"){
+			typeName = words[1];
+			// cleanup field and constraint info
+		} else if(words[0] == "Fields" && words[1] == "{"){
+			state = parseFieldList;
+		} else if(words[0] == "Constraints" && words[1] == "{"){
+			state = parseConstraintList;
+		}
+	}
+
+	void parse(const StringChunk & line)
+	{
+		state(line);
+	}
+};
+
+class Generator
+{
+	public:
+		void include_CommonMarkerValues(StringChunk & tpl)
+		{
+			if(tpl.chopFront("@Type@")) {
+				tpl.trimFront("\n\r");
+				//code.trimBack("\t");
+				code << typeName;
+			} else {
+				code << tpl[0];
+				tpl.chopFront(1);
+			}
+		}
+
+		void include_foreachField(StringChunk & tpl, const Array<FieldDesc> & fields)
+		{
+			unsigned pos = 0;
+			for(unsigned i = 0; i < fields.length; i++){
+				if(*tpl.data != '@'){
+					code << *tpl.data;
+					tpl.chopFront(1);
+				}
+				if(tpl.chopFront("@Type@")) {
+					tpl.trimFront("\n\r");
+					//code.trimBack("\t");
+					code << typeName;
+				} else if(tpl.chopFront("@FieldName@")){
+					code << fields[i].name;
+				} else if(tpl.chopFront("@SqlDecl@")){
+					if(fields[i].name.length)
+						code << "`" << fields[i].name << "` ";
+					code << fields[i].sqlDecl;
+				} else
+					include_CommonMarkerValues(tpl);
+			}
+		}
+
+		void processTemplateLine(StringChunk & tpl)
+		{
+			unint pos;
+			if(tpl.chopFront("@ForEachFieldBegin@")){
+				tpl.trimFront("\n\r");
+				code.trimBack("\t");
+				/*$sectionPos = tpl.find(pos, "@ForEachFieldEnd@");
+				  $section = rtrim(substr($tpl, $pos, $sectionPos-$pos), "\t");
+				  $pos = $sectionPos + 17;
+				  include_skip_newlines($tpl, $pos);
+				  $sectionLast = strstr($section, "@ForEachFieldLast@");
+				  if($sectionLast !== false){
+				  $sectionLast = ltrim(substr($sectionLast, 18), "\n\r");
+				  $section = rtrim(strstr($section, "@ForEachFieldLast@", true), "\t");
+				  }*/
+				/*                unsigned last = count(classFields) - 1;
+						  for(int i = 0; i <= last; i++)
+						  if(last == i && sectionLast !== false)
+						  include_foreachField(sectionLast, classFields[i]);
+						  else
+						  include_foreachField(section, classFields[i]);
+						  */
+			} else
+				include_CommonMarkerValues(tpl);
+		}
+
+		void parse(const StringChunk & tpl)
+		{
+		}
+};
+
+class InputCode
+{
+	void (const StringChunk &) state;
+
+	StringChunk tplDir;
+	Declaration declaration;
+
 	String code;
 
-Generator(const StringChunk & tplDir) :
-	inputState(InputState::Start),
-	templateState(TemplateState::Start),
-	tplDir(tplDir)
-{
-}
-
-void include_CommonMarkerValues(StringChunk & tpl)
-{
-	if(tpl.chopFront("@Type@")) {
-		tpl.trimFront("\n\r");
-		//code.trimBack("\t");
-		code << typeName;
-	} else {
-		code << tpl[0];
-		tpl.chopFront(1);
+public:
+	InputCode(const StringChunk & tplDir) :
+		state(parseCode),
+		tplDir(tplDir)
+	{
 	}
-}
 
-void include_foreachField(StringChunk & tpl, const Array<FieldDesc> & fields)
-{
-	unsigned pos = 0;
-	for(unsigned i = 0; i < fields.length; i++){
-		if(*tpl.data != '@'){
-			code << *tpl.data;
-			tpl.chopFront(1);
+	void parseCode(const StringChunk & line)
+	{
+		unint pos;
+		if(!line.findFirst("@")){
+			code << line << "\n";
+			return;
 		}
-		if(tpl.chopFront("@Type@")) {
-			tpl.trimFront("\n\r");
-			//code.trimBack("\t");
-			code << typeName;
-		} else if(tpl.chopFront("@FieldName@")){
-			code << fields[i].name;
-		} else if(tpl.chopFront("@SqlDecl@")){
-			if(fields[i].name.length)
-				code << "`" << fields[i].name << "` ";
-			code << fields[i].sqlDecl;
+		if(line.findFirst("@BeginDecl@")){
+			declaration.clear();
+			state = parseDeclaration;
+		} else if(line.findFirst(pos, "@include@")){
+			pos += 10;
+			StringChunk includeList(line.data + pos, line.length - pos);
+			parseInclude(includeList);
 		} else
-			include_CommonMarkerValues(tpl);
+			code << line << "\n";
 	}
-}
 
-void processTemplateLine(StringChunk & tpl)
-{
-	unint pos;
-        if(tpl.chopFront("@ForEachFieldBegin@")){
-		tpl.trimFront("\n\r");
-                code.trimBack("\t");
-                /*$sectionPos = tpl.find(pos, "@ForEachFieldEnd@");
-                $section = rtrim(substr($tpl, $pos, $sectionPos-$pos), "\t");
-                $pos = $sectionPos + 17;
-                include_skip_newlines($tpl, $pos);
-                $sectionLast = strstr($section, "@ForEachFieldLast@");
-                if($sectionLast !== false){
-                        $sectionLast = ltrim(substr($sectionLast, 18), "\n\r");
-                        $section = rtrim(strstr($section, "@ForEachFieldLast@", true), "\t");
-                }*/
-/*                unsigned last = count(classFields) - 1;
-                for(int i = 0; i <= last; i++)
-                        if(last == i && sectionLast !== false)
-                                include_foreachField(sectionLast, classFields[i]);
-                        else
-                                include_foreachField(section, classFields[i]);
-				*/
-	} else
-		include_CommonMarkerValues(tpl);
-}
-
-void readDeclaration(StringChunk & line)
-{
-	unint pos;
-	switch(state){
-		case InputState::Declaration :
-			{
-			auto words = line.split(" \t");
-			if(words[0] == "Class"){
-				typeName = words[1];
-				// cleanup field and constraint info
-			} else if(words[0] == "Fields" && words[1] == "{"){
-				state = InputState::FieldList;
-			} else if(words[0] == "Constraints" && words[1] == "{"){
-				state = InputState::ConstraintList;
-			}
-			}
-			break;
-		case InputState::FieldList :
-			{
-			StringChunk comment;
-			if(line.findFirst(pos, "//")){
-				comment.assign(line.data + pos, line.length - pos);
-				line.cutAt(pos);
-			}
-			auto words = line.split(";");
-			if(words.length == 1 && words[0] == "}"){
-				state = InputState::Declaration;
-				break;
-			}
-			
-			StringChunk type;
-			StringChunk name;
-			if(0 < words.length){
-				words[0].trim(" \t");
-				if(words[0].findLastOf(pos, " \t")){
-					type.assign(words[0].data, pos);
-					words[0].chopFront(pos + 1);
-					name.assign(words[0]);
-				}
-			}
-
-			bool key = false;
-			if(1 < words.length){
-				auto modifiers = words[1].split(",");
-				key = modifiers.has("key");
-			}
-
-			StringChunk sql;
-			if(2 < words.length)
-				sql = words[2];
-
-			fields.add(name, type, sql, comment);
-			if(key)
-				keyFields.add(name, type, sql, comment);
-			else
-				nonKeyFields.add(name, type, sql, comment);
-			}
-			break;
-		case InputState::ConstraintList :
-			{
-			auto words = line.split(";");
-			if(words.length == 1 && words[0] == "}"){
-				state = InputState::Declaration;
-				break;
-			}
-			
-			line.trim("\n ");
-			constraints.add(line);
-			}
-			break;
-		default:
-			break;
+	void parseDeclaration(const StringChunk & line)
+	{
+		if(line.findFirst(pos, "@EndDecl@")){
+			state = parseCode;
+		} else
+			declaration.parse(line);
 	}
-}
 
-void inputLine(StringChunk & line)
-{
-	unint pos;
-	switch(state){
-		case InputState::Declaration :
-			if(line.findFirst(pos, "@EndDecl@")){
-				line.chopFront(pos + strlen("@EndDecl@"));
-				code << line;
-				line.clear();
-				state = InputState::Start;
-			} else
-				readDeclaration(line);
-			code << line;
-			break;
-		default :
-			if(line.findFirst(pos, "@")){
-				code << line;
-				line.chopFront(pos);
-				if(line.chopFront("@BeginDecl@")){
-					code.append("@BeginDecl@");
-					state = InputState::Declaration;
-				} else if(line.chopFront("@include@")){
-					auto files = line.split(" ");
-					for(unsigned i = 0; i < files.length; i++){
-						String tplFileName(tplDir);
-						tplFileName << files[i] << ".cpp";
-						File tplFile(tplFileName);
-						String tpl = tplFile.readAll();
-						tpl.replace("\r", "");
-						auto lines = tpl.split("\n");
-						unsigned j;
-						try{
-							for(j = 0; j < lines.length; j++){
-								lines[j].trimBack("\t ");
-								processTemplateLine(lines[j]);
-							}
-						} catch (Exception & e) {
-							e.note("Excpetion happened while processing file %s line %u.", tplFileName.str, j);
-							EXCEPTION(e);
-							break;
-						}
-					}
-				}
-			} else
-				code << line;
+	void parseInclude(const StringChunk & includeList)
+	{
+		auto files = includeList.split(" ");
+		for(unsigned i = 0; i < files.length; i++){
+			String tplFileName(tplDir);
+			tplFileName << files[i] << ".cpp";
+			File tplFile(tplFileName);
+			String tpl = tplFile.readAll();
+			StringChunk chunk(tpl);
+			TemplateParser tplParser;
+			tplParser.parse(chunk);
+		}
 	}
-	code << "\n";
-}
 
+	void parse(const StringChunk & line)
+	{
+		state();
+	}
 };
 
 
@@ -319,7 +323,7 @@ int main(int argc, char *args[])
 		return 1;
 	}
 
-	Generator generator(tplDir);
+	InputCode generator(tplDir);
 	File inputFile(inputFileName);
 	String input = inputFile.readAll();
 	input.replace("\r", "");
@@ -328,7 +332,7 @@ int main(int argc, char *args[])
 	try{
 		for(i = 0; i < lines.length; i++){
 			lines[i].trimBack("\t ");
-			generator.inputLine(lines[i]);
+			generator.parse(lines[i]);
 		}
 	} catch (Exception & e) {
 		e.note("Excpetion happened while processing file %s line %u.", inputFileName.data, i);
